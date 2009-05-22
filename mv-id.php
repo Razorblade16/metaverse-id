b0ee02606ab7e1f6a169b9b6c533412a4e81fbcd
SignpostMarv <me@signpostmarv.name>
2009-05-22 13:57:21 +0000

    forgot to update version number :-s
    git-svn-id: http://svn.wp-plugins.org/metaverse-id/tags/0.6.1@119716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mv-id.php
+++ mv-id.php
@@ -3,7 +3,7 @@
 Plugin Name: Metaverse ID
 Plugin URI: http://blog.signpostmarv.name/mv-id/
 Description: Display your identity from around the metaverse!
-Version: 0.6
+Version: 0.6.1
 Author: SignpostMarv Martin
 Author URI: http://blog.signpostmarv.name/
  Copyright 2009 SignpostMarv Martin  (email : mv-id.wp@signpostmarv.name)
